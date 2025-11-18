92b13bd87121503bf4e7bd65a6e31bd411270619
K.Vamshi Krishna <121361572+vamshikrishna970@users.noreply.github.com>
2025-11-19 00:27:21 +0530

    count ways nqueens

---
+++ DSA/n-queens-countways.java
@@ -0,0 +1,41 @@
+/Count ways to solve N-Queens problem
+public class classroom{
+    public static void nqueens(char board[][], int row){
+        //base case
+        if(row==board.length){
+            //printBoard(board);
+            count++;
+            return;
+        }
+
+        //column loop
+        for(int j=0;j<board.length;j++){
+            if(isSafe(board,row,j)){
+                board[row][j]='Q';
+                nqueens(board,row+1);//function call
+                board[row][j]='X';//backtracking step
+            }
+        }
+    }
+    public static void printBoard(char board[][]){
+        System.out.println("-----Chess Board-----");
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board.length;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+        static int count=0;
+    }public static void main(String args[]){
+        int n=4;
+        char board[][]=new char[n][n];
+        //initialize the chess board
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                board[i][j]='X';
+            }
+        }
+        nqueens(board,0);
+        System.out.println("Total ways to solve N-Queens: "+count);
+    }
+}
