7b93316a478f3654e040358a9836b8da15cbbb3c
vamshikrishna970 <121361572+vamshikrishna970@users.noreply.github.com>
2025-11-18 18:05:35 +0530

    Implement permutation finder in findpermutation.java

---
+++ findpermutation.java
@@ -0,0 +1,19 @@
+public class classroom {
+    public static void findpermutation(string str, String ans) {
+        // base case
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+        // recursive call
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String newstr = str.substring(0, i) + str.substring(i + 1);
+            findpermutation(newstr, ans + curr);
+        }
+    }
+}
+public static void main(String[] args) {
+    String str = "abc";
+    findpermutation(str, "");
+}
