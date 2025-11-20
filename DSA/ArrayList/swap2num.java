55496acd17f047f113f1afed7990ef08d312f4f7
K.Vamshi Krishna <121361572+vamshikrishna970@users.noreply.github.com>
2025-11-20 23:36:38 +0530

    swap two num using array

---
+++ DSA/ArrayList/swap2num.java
@@ -0,0 +1,20 @@
+import java .util.ArrayList;
+public class swap2num {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        System.out.println("Before swapping: " + list);
+        swap(list, 1, 3);
+        System.out.println("After swapping: " + list);
+    }
+
+    public static void swap(ArrayList<Integer> list, int index1, int index2) {
+        int temp = list.get(index1);
+        list.set(index1, list.get(index2));
+        list.set(index2, temp);
+    }
+}
