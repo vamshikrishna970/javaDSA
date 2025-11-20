782b59259583538f6b5ea859f5df4f81da52d0a7
K.Vamshi Krishna <121361572+vamshikrishna970@users.noreply.github.com>
2025-11-21 01:51:31 +0530

    Add files via upload

---
+++ DSA/ArrayList/maxwater2pointer.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class maxwater2pointer {
+    public static int storedWater(ArrayList<Integer> height) {
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.size() - 1;
+        while (lp < rp) {
+            // calculate water area
+            int ht = Math.min(height.get(lp), height.get(rp));
+            int width = rp - lp;
+            int currentWater = ht * width;
+            maxWater = Math.max(maxWater, currentWater);
+            // update pointers
+            if (height.get(lp) < height.get(rp)) {
+                lp++;
+            } else {
+                rp--;
+            }
+        }
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+
+        int result = storedWater(height);
+        System.out.println("Maximum water that can be stored: " + result);
+    }
+}
